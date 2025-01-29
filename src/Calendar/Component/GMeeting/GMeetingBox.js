fcf6ffc92453e1eeb0dfa4704bbd1f7edb7c010a
Gunaselvam <guna.selvam@securekloud.com>
2025-01-29 07:18:22 +0530

    added missing value

--- src/Calendar/Component/GMeeting/GMeetingBox.js
+++ src/Calendar/Component/GMeeting/GMeetingBox.js
@@ -13,67 +13,60 @@ import Google from "../../Icons/google_logo.png";
 import { calendarMeeting } from "../../CalendarData/CalendarData";
 
 const GMeeting = ({ data, open, close }) => {
-  let Round = `Interview Round : ${data?.summary}`;
-  let Date = data?.start
-    ? `Interview Date : ${dayjs(data.start).format("Do MMM YYYY")}`
-    : null;
-  let Time =
-    data?.start && data?.end
-      ? `Interview Time : ${dayjs(data.start).format("hh:mm A")} to ${dayjs(
-          data.end
-        ).format("hh:mm A")}`
-      : null;
-  let Interviewer = `Interview with : ${data?.user_det?.candidate?.candidate_firstName}`;
+  const meetingDetails = [
+    { label: "Position:", value: data?.user_det?.job_id?.jobRequest_Title },
+    { label: "Interview Round:", value: data?.summary },
+    { label: "Created By:", value: data?.user_det?.job_id?.jobRequest_createdBy || "Camila lopez" },
+    { label: "Interview Date:", value: data?.start ? dayjs(data.start).format("Do MMM YYYY") : "N/A" },
+    { label: "Interview Time:", value: data?.start && data?.end ? `${dayjs(data.start).format("hh:mm A")} to ${dayjs(data.end).format("hh:mm A")}` : "N/A" },
+    { label: "Interview with:", value: data?.user_det?.candidate?.candidate_firstName },
+    { label: "Interview via:", value: "Google Meet" },
+  ];
+
   const meetingLink = calendarMeeting.link;
-
-
   const openLink = () => {
-    window.open(meetingLink)
-}
+    window.open(meetingLink);
+  };
 
   return (
-    <>
-           <Dialog open={open} onClose={() => close(false)} maxWidth="sm" fullWidth>
-        <DialogTitle>
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <div></div>
-            <div>
-              <Close onClick={() => close(false)} />
-            </div>
-          </div>
-        </DialogTitle>
-        <DialogContent>
-          <Box display="flex" gap={2}>
-            <Box
-              className="popup-left"
-              display="flex"
-              flexDirection="column"
-              alignItems="center"
-              gap={2}
+    <Dialog open={open} onClose={() => close(false)} maxWidth="sm" fullWidth>
+      <DialogTitle>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div className="meeting-title">Meeting Info</div>
+          <Close onClick={() => close(false)} style={{ cursor: "pointer" }} />
+        </div>
+      </DialogTitle>
+      <DialogContent>
+        <Box display="flex" gap={2}>
+          <Box
+            className="popup-left"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            gap={2}
+          >
+            <img src={Google} width={100} height={100} alt="Google Meet" />
+            <Button
+              variant="contained"
+              color="primary"
+              className="join-button"
+              onClick={openLink}
             >
-              <img src={Google} width={100} height={100} />
-
-              <Button
-                variant="contained"
-                color="primary"
-                className="join-button"
-                onClick={openLink}
-              >
-                Join
-              </Button>
-            </Box>
-
-            <Box className="popup-right">
-              <div className="meeting-title">Meeting Info</div>
-              <div className="meeting-info">{Round}</div>
-              <div className="meeting-info">{Date}</div>
-              <div className="meeting-info">{Time}</div>
-              <div className="meeting-info">{Interviewer}</div>
-            </Box>
+              Join
+            </Button>
           </Box>
-        </DialogContent>
-      </Dialog>
-    </>
+
+          <Box className="popup-right">
+            {meetingDetails.map((item, index) => (
+              <div className="meeting-info" key={index}>
+                <span className="label">{item.label} </span>
+                <span className="value">{item.value}</span>
+              </div>
+            ))}
+          </Box>
+        </Box>
+      </DialogContent>
+    </Dialog>
   );
 };
 
